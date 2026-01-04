7a345ed8c8dad05af7f71ee6fef31ed954293ac9
RaffyLeong <raffy36249@gmail.com>
2026-01-04 23:01:20 +0800

    polished layout structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 interface AnalysisType {
   match: string;
   techSKill: string[];
-
   requirements: string[];
   suggestions: string[];
 }
@@ -18,16 +17,52 @@ function App() {
   });
 
   const mySkills = [
-    "JavaScript", "React", "Git", "TypeScript", "CSS", "Tailwind CSS", "EHR", "Appointment Booking Software"
+    "JavaScript",
+    "React",
+    "Git",
+    "TypeScript",
+    "CSS",
+    "Tailwind CSS",
+    "EHR",
+    "Appointment Booking Software",
   ];
 
-  const techSkills = [ 
-    "React", "TypeScript", "JavaScript","Python","Java", "C#", "Go", "Node.js","Express","Next.js","Vue",
-    "Angular", "Tailwind CSS", "CSS", "SCSS", "Bootstrap", "HTML", "MongoDB", "PostgreSQL", "MySQL", "AWS", "Docker",
-    "Git", "REST", "GraphQL", "Redux", "Jest", "React Native", "Flutter", "Dart", "Swift", "Figma", "Vite", "REST API",
+  const techSkills = [
+    "React",
+    "TypeScript",
+    "JavaScript",
+    "Python",
+    "Java",
+    "C#",
+    "Go",
+    "Node.js",
+    "Express",
+    "Next.js",
+    "Vue",
+    "Angular",
+    "Tailwind CSS",
+    "CSS",
+    "SCSS",
+    "Bootstrap",
+    "HTML",
+    "MongoDB",
+    "PostgreSQL",
+    "MySQL",
+    "AWS",
+    "Docker",
+    "Git",
+    "REST",
+    "GraphQL",
+    "Redux",
+    "Jest",
+    "React Native",
+    "Flutter",
+    "Dart",
+    "Swift",
+    "Figma",
+    "Vite",
+    "REST API",
   ];
-
-
 
   const handleAnalyzeButton = () => {
     // if text is empty or has white space than return
@@ -38,8 +73,7 @@ function App() {
       text.toLowerCase().includes(skill.toLowerCase())
     );
 
-
-    const allRequireSkills = [...requireSkills]
+    const allRequireSkills = [...requireSkills];
 
     // look though for each skill in MySkills and pick one skill that has in the requireSkills
     const matchSkills = mySkills.filter((skill) =>
@@ -51,10 +85,12 @@ function App() {
       (skill) => !mySkills.includes(skill)
     );
 
-    const techMatch = requireSkills.length > 0 ? (matchSkills.length / requireSkills.length) * 100 : 0
-  
+    const techMatch =
+      requireSkills.length > 0
+        ? (matchSkills.length / requireSkills.length) * 100
+        : 0;
 
-    const totalMatch = Math.round(techMatch)
+    const totalMatch = Math.round(techMatch);
 
     // split it each sentence for each line, and Filter each line for keywords like "experience", "skill", etc.
     // and found the first 5 sentence that has those keyword
@@ -74,18 +110,19 @@ function App() {
       )
       .slice(0, 5);
 
-      // Suggestion
+    // Suggestion
     // start with empty, if more than 1 showing what skill u need to focus, if none display great match
-    const suggestions = []
+    const suggestions = [];
 
     if (missingSkills.length > 0) {
-      suggestions.push(`Technical: Focus on ${missingSkills.slice(0, 2).join(", ")}`)
+      suggestions.push(
+        `Technical: Focus on ${missingSkills.slice(0, 2).join(", ")}`
+      );
     }
 
     if (missingSkills.length === 0) {
-      suggestions.push("Great match! Focus behavioral questions")
+      suggestions.push("Great match! Focus behavioral questions");
     }
-      
 
     // Overall the Analysis state - match, techStack, requirement and suggestion
     setAnalysis({
@@ -113,24 +150,80 @@ function App() {
   return (
     <div className="min-h-screen p-4 mb-10 md:p-8">
       {/* Header */}
-      <header className="max-w-6xl mx-auto mb-10">
-        <h1 className="text-[36px] font-bold text-black mb-4">
+      <header className="max-w-6xl mx-auto mb-10 pb-8 border-b border-gray-200">
+        <h1 className="text-[36px] md:text-5xl font-bold text-gray-900 mb-4">
           Job Post Analyzer
         </h1>
-        <p className="text-[24px] text-black mt-2">
-          Paste any job description. Get instant insights on requirements and
-          tech stack.
+
+        <p className="text-[24px] md:text-xl text-gray-600 max-w-3xl mt-2">
+          Paste any job description. Get instant insights on requirements
         </p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <div className="w-12 h-1 bg-blue-500 rounded-full"></div>
+          <div className="w-6 h-1 bg-blue-300 rounded-full"></div>
+          <div className="w-3 h-1 bg-blue-200 rounded-full"></div>
+        </div>
       </header>
-      <div className="mb-10 max-w-6xl mx-auto flex flex-col gap-4">
+
+      <div className="mt-6 max-w-6xl mx-auto flex flex-col gap-4 ">
         {/* How to use */}
         <div className="mb-2">
-          <h2 className="text-2xl font-semibold mb-2">How to Use</h2>
-          <ol className="list-decimal list-inside text-gray-700">
-            <li>Copy a job description from any job posting.</li>
-            <li>Paste above and click "Analyze"</li>
-            <li>See required tech stack instantly</li>
-            <li>Get personalized study suggestions</li>
+          <h2 className="text-[32px] md:text-4xl font-semibold mb-2">
+            How to Use
+          </h2>
+
+          <div className="mt-6 flex items-center gap-2">
+            <div className="w-12 h-1 bg-blue-500 rounded-full"></div>
+            <div className="w-6 h-1 bg-blue-300 rounded-full"></div>
+            <div className="w-3 h-1 bg-blue-200 rounded-full"></div>
+          </div>
+
+          <ol className="space-y-4 mt-6 rounded-2xl">
+            <li className="flex item-start gap-4">
+              <div
+                className="flex-shrink-0 w-8 h-8 bg-blue-600 text-white rounded-full 
+              flex items-center justify-center font-bold"
+              >
+                1
+              </div>
+              <span className="font-medium text-[18px] text-blue-600">
+                Copy a job description
+              </span>
+            </li>
+            <li className="flex item-start gap-4">
+              <div
+                className="flex-shrink-0 w-8 h-8 bg-blue-600 text-white rounded-full 
+              flex items-center justify-center font-bold"
+              >
+                2
+              </div>
+              <span className="font-medium text-[18px] text-blue-600">
+                Paste below
+              </span>
+            </li>
+            <li className="flex item-start gap-4">
+              <div
+                className="flex-shrink-0 w-8 h-8 bg-blue-600 text-white rounded-full 
+              flex items-center justify-center font-bold"
+              >
+                3
+              </div>
+              <span className="font-medium text-[18px] text-blue-600">
+                Click "Analyze"
+              </span>
+            </li>
+            <li className="flex item-start gap-4">
+              <div
+                className="flex-shrink-0 w-8 h-8 bg-blue-600 text-white rounded-full 
+              flex items-center justify-center font-bold"
+              >
+                4
+              </div>
+              <span className="font-medium text-[18px] text-blue-600">
+                Get personalized study suggestions
+              </span>
+            </li>
           </ol>
         </div>
 
@@ -146,13 +239,13 @@ function App() {
         <div className="flex gap-3 mt-4">
           <button
             onClick={handleAnalyzeButton}
-            className="bg-blue-500 text-white rounded p-2 hover:bg-blue-600 transition"
+            className="bg-blue-600 text-white rounded p-2 hover:bg-blue-800 transition"
           >
             Analyze this Job
           </button>
           <button
             onClick={handleClearButton}
-            className="bg-blue-500 text-white rounded p-2 hover:bg-blue-600 transition"
+            className="bg-blue-600 text-white rounded p-2 hover:bg-blue-800 transition"
           >
             Clear
           </button>
@@ -166,25 +259,49 @@ function App() {
       </div>
 
       {/* Results */}
-      <div className="max-w-6xl mx-auto">
-        <h2 className="text-2xl font-semibold mb-4">Analysis Results</h2>
+      <div className="mt-20 max-w-6xl mx-auto">
+        <h2 className="text-[32px] md:text-4xl font-semibold mb-4">
+          Analysis Results
+        </h2>
+        <div className="mt-6 flex items-center gap-2">
+          <div className="w-12 h-1 bg-blue-500 rounded-full"></div>
+          <div className="w-6 h-1 bg-blue-300 rounded-full"></div>
+          <div className="w-3 h-1 bg-blue-200 rounded-full"></div>
+        </div>
 
         {/* if analysis.match is true then show the code OR show Empty state */}
         {analysis.match ? (
           <div className="space-y-6">
             {/* Match Score */}
             <section>
-              <div className="flex gap-2 items-center mb-2">
-                <h1 className=" text-gray-900 font-bold">Role Suitability:</h1>
-                <p className="text-gray-700">{analysis.match}%</p>
+              <div className="mt-10 mb-8">
+                <h1 className="text-[22px] text-blue-600 font-bold mb-3">
+                  Role Suitability
+                </h1>
+                <div className="flex flex-col items-start gap-4 mb-3">
+                  <span
+                    className={`mt-4 font-bold text-3xl md:text-4xl ${
+                      parseInt(analysis.match) >= 50
+                        ? "text-green-600"
+                        : "text-red-600"
+                    }`}
+                  >
+                    {analysis.match}%
+                  </span>
+                </div>
               </div>
             </section>
 
             {/* Tech Skill */}
-            <section>
-              <div className="flex gap-2 items-center mb-2">
-                <h1 className="text-gray-900 font-bold">Required Tech Skills:</h1>
-                <div className="flex flex-wrap gap-2">
+            <section >
+              <div className="flex flex-col gap-2">
+                <h1 className="text-[22px] text-blue-600 font-bold">
+                  Required Tech Skills
+                  <span className="text-gray-600 text-sm font-normal ml-2">
+                   ({analysis.techSKill.length} found)
+                  </span>
+                </h1>
+                <div className="mt-6 flex flex-wrap gap-2">
                   {analysis.techSKill.length > 0 ? (
                     analysis.techSKill.map((skill, index) => {
                       const hasSkill = mySkills.includes(skill);
@@ -194,7 +311,7 @@ function App() {
                           className={`px-4 py-2 rounded-lg font-medium border ${
                             hasSkill
                               ? "bg-white text-green-600 "
-                              : "bg-white text-red-800 "
+                              : "bg-white text-red-600 "
                           }`}
                         >
                           {skill}
@@ -212,25 +329,57 @@ function App() {
 
             {/* Requirements */}
             <section>
-              <div className="flex flex-col gap-2 items-start">
-                <h1 className=" text-gray-900 font-bold">Key Requirements:</h1>
-                <ul className="text-gray-700 list-disc list-inside space-y-2 mt-5">
+              <div className="flex flex-col gap-2">
+                <h1 className="text-[22px] text-blue-600 font-bold">
+                  Key Requirements
+                   <span className="text-gray-600 text-sm font-normal ml-2">
+                   ({analysis.requirements.length} found)
+                  </span>
+                </h1>
+                
+                <div className="rounded-xl p-4 border border-gray-200">
+                <ul className="space-y-4">
                   {analysis.requirements.map((req, index) => {
-                    return <li key={index}>{req}</li>;
+                    return <li key={index} className="flex items-start">
+                      <div className="w-6 h-6 bg-blue-600 text-white font-bold rounded-full 
+                           flex items-center justify-center mr-3 mt-1 flex-shrink-0">
+                        {index + 1}
+                      </div>
+                      <span className="text-gray-700 font-medium text-[18px] leading-relaxed">{req}</span>
+                      </li>;
                   })}
                 </ul>
+                </div>
               </div>
             </section>
 
             {/* Suggestions */}
             <section>
-              <div className="flex gap-2 items-center">
-                <h1 className=" text-gray-900 font-bold mt-4">Study Suggestions:</h1>
-                
-                  {analysis.suggestions.map((suggestion, index) =>
-                    <p key={index} className="text-red-800 font-bold mt-4">{suggestion}</p>
-                  )}
-                
+              <div className="flex flex-col gap-2">
+                <h1 className="text-[22px] text-blue-600 font-bold mt-4">
+                  Study Suggestions
+                  <span className="text-gray-600 text-sm font-normal ml-2">
+                   ({analysis.suggestions.length} found)
+                  </span>
+                </h1>
+
+                <div className="p-6 rounded-xl border text-[18px]" >
+                 {analysis.suggestions.map((suggestion, index) => (
+                   <p
+                     key={index}
+                     className={`flex items-start font-medium ${
+                      parseInt(analysis.match) >= 50 
+                      ? "text-green-600" 
+                      : "text-red-600"
+                     }`}
+                   >
+                    <div className="flex items-center justify-center mr-4">
+                      {parseInt(analysis.match) >= 50 ? "💡" : "📚"}
+                    </div>
+                     {suggestion}
+                   </p>
+                 ))}
+                </div>
               </div>
             </section>
           </div>
@@ -241,7 +390,8 @@ function App() {
               No Job Analyzed Yet
             </h3>
             <p className="text-gray-600 mb-4">
-              Paste a job description above and click "Analyze this job" button to see your match score.
+              Paste a job description above and click "Analyze this job" button
+              to see your match score.
             </p>
           </div>
         )}
